e1e1828e1c49e3d94a3fd7437c7e4769cd8e4810
Chris <chris@xenforo.com>
2020-02-27 12:30:32 +0000

    Minor change to revert an undesirable change - Pikaday/Pikaday#720 - which made direct text entry of dates more difficult as backspace clears the contents entirely.

--- pikaday.js
+++ pikaday.js
@@ -564,10 +564,6 @@
                     case 40:
                         self.adjustDate('add', 7);
                         break;
-                    case 8:
-                    case 46:
-                        self.setDate(null);
-                        break;
                 }
             }
         };
